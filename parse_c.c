29add531ced2946e6c0994d4b50ed8719b477e0a
NyamburaKariuki <prettimercii@gmail.com>
2023-12-11 11:53:55 +0300

    Command parse file

---
+++ parse_c.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *command_parse - function to parse arguments of a command
+ *@status: status
+ *Return: Always 0 (Success)
+ */
+
+char **comand_parse(const char *status)
+{
+	char **args;
+	args = malloc(2 * sizeof(char *));
+	args[0] = strdup(status);
+	args[1] = NULL;
+	return (args);
+}
--- prompt.c
+++ prompt.c
@@ -1,4 +1,8 @@
 #include "shell.h"
+/**
+ *show_prompt - function to show the prompt of a shell 
+ *Return: Always 0 (Success)
+ */
 void show_prompt(void)
 {
 	show_print("shell $ ");
