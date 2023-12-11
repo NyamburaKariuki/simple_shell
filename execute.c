c99a02bc7b4bd939cf3c7315be95fb604a5f40eb
Nickhongo <hongonicholas0@gmail.com>
2023-12-11 11:57:18 +0300

    Execution

---
+++ execute.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+void execute_command(char **args)
+{
+        if (args[0] != NULL && strcmp(args[0], "exit") == 0)
+        {
+                for (int j = 0; args[j] != NULL; j++)
+                {
+                        free(args[j]);
+                }
+                free(args);
+                exit(0);
+        }
+
+        pid_t pid_child = fork();
+
+        if (pid_child == -1)
+        {
+                s_error("forking error");
+                exit(1);
+        }
+        else if (pid_child == 0)
+        {
+                execvp(args[0], args);
+                show_print("No such file or directory.\n");
+                exit(1);
+        }
+        else
+        {
+                wait(NULL);
+        }
+}
