5b633a56ab8673ca25a090834573ad26da61b565
NyamburaKariuki <prettimercii@gmail.com>
2023-12-14 19:01:44 +0300

    String functions

---
+++ string_functions.c
@@ -0,0 +1,49 @@
+#include "shell.h"
+/**
+  *_strcopy - function to to copy a string
+  *@src: source string
+  *@dest: destination
+  *Return: Always 0 (Success)
+  */
+char *_strcopy(char *src, char *dest)
+{
+	int x = 0;
+
+	while (src[x])
+	{
+	dest[x] = src[x];
+	x++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
+/**
+  *_catoi - function to convert a string to an integer
+  *@str: string to conevrt
+  *Return: Always 0 (Success)
+  */
+int _catoi(char *str)
+{
+	int x = 0, y = 1, z;
+	unsigned int p = 0;
+
+	while (str[x] != '\0')
+	{
+		if (str[x] == '-')
+		{
+			return (2);
+		}
+		z = str[x] - '0';
+
+		if (p > 0 && !(z >= 0 && z <= 9))
+			break;
+
+		if (z >= 0 && z <= 9)
+			p = p * 10 + z;
+
+		x++;
+	}
+	p *= y;
+	return (p);
+}
+
